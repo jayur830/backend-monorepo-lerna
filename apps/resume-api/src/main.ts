1166e466ad6d3a797cdd83624397fe70d80f3da0
jayur830 <jayur830@naver.com>
2024-02-28 00:27:21 +0900

    ci(resume-api): 빌드 테스트

--- apps/resume-api/src/main.ts
+++ apps/resume-api/src/main.ts
@@ -3,10 +3,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app/app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
-  const port = process.env.PORT;
-  const env = process.env.NODE_ENV;
-  await app.listen(port);
   console.log(
     `\n${JSON.stringify(
       {
@@ -21,6 +17,10 @@ async function bootstrap() {
       2,
     )}\n`,
   );
+  const app = await NestFactory.create(AppModule, { cors: true });
+  const port = process.env.PORT;
+  const env = process.env.NODE_ENV;
+  await app.listen(port);
   console.log('Environment:', env);
   console.log(`Server is listening port ${port}.`);
 }
