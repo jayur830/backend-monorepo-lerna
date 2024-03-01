9270eedb9be1a6064f5932e8faaff45dd68d2d29
jayur830 <jayur830@naver.com>
2024-03-02 03:59:18 +0900

    fix(resume-api): `console.log` 삭제

--- apps/resume-api/src/main.ts
+++ apps/resume-api/src/main.ts
@@ -3,21 +3,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app/app.module';
 
 async function bootstrap() {
-  console.log(
-    'mysql connection:',
-    `\n${JSON.stringify(
-      {
-        type: 'mysql',
-        host: process.env.MYSQL_HOST,
-        port: +process.env.MYSQL_PORT,
-        username: process.env.MYSQL_USERNAME,
-        password: process.env.MYSQL_PASSWORD,
-        database: process.env.MYSQL_DATABASE,
-      },
-      null,
-      2,
-    )}\n`,
-  );
   const app = await NestFactory.create(AppModule, { cors: true });
   const port = process.env.PORT;
   const env = process.env.NODE_ENV;
